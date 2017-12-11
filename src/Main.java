91ef0161e6e2511eb74286aba0d57687ea011bff
61672 <rdeller.61672@farnborough.ac.uk>
2017-12-11 10:20:25 +0000

    Initial commit-Can't compile

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class Main {
+
+
+    public static void main(String args){
+        public int[] numbers = new int{2,6,3,1,2};
+        mergeSort(numbers);
+
+    }
+
+    public static int[] mergeSort(int[] numbers){
+        if (numbers.length <= 1){
+            return numbers;
+        }
+        int left = [];
+        int right = [];
+        for (i : numbers){
+
+        }
+
+
+        return numbers;
+    }
+
+    public static int[] quickSort(int[] numbers){
+
+
+
+    return numbers;
+    }
+}
